b344c20f586fc353ee119ea545724617cad97f4f
Junseong Woo <woojs0505@naver.com>
2021-11-09 10:24:39 +0900

    Create dfs.py

---
+++ dfs.py
@@ -0,0 +1,22 @@
+def dfs(v):
+    visited[v] = 1 # true
+    print(v, end=' ')
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(i)
+
+# 인접행렬
+graph = [
+    [],
+    [2, 3, 8],
+    [1, 7],
+    [1, 4, 5],
+    [3, 5],
+    [3, 4],
+    [7],
+    [2, 6, 8],
+    [1, 7]
+]
+
+visited = [0] * 9
+dfs(1)
